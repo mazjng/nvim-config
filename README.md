14744be347d5534bf1e77b5b3f1eeb9b633ffc7d
marcus <marcusngzhijie@gmail.com>
2026-09-15 18:26:31 +0800

    improved tree-sitter / LSP portability

---
+++ README.md
@@ -0,0 +1,6 @@
+# Neovim config 
+
+## Obstructions to drop ins
+
+- nvim-treesitter has a dependancy `tree-sitter-cli` that must be installed
+separately. its on pacman and winget.
--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,21 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
+    opts = {
+        ensure_installed = {
+            "bashls",
+            "clangd",
+            "cssls",
+            "eslint",
+            "html",
+            "lua_ls",
+            "marksman",
+            "powershell_es",
+            "pyright",
+            "ruff",
+            "texlab",
+            "ts_ls",
+        }
+    },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
         "neovim/nvim-lspconfig",
