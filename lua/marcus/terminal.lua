df8251b3155a6e1f56dcba26e77cd52e821cc4d2
marcus <marcusngzhijie@gmail.com>
2026-09-15 14:12:01 +0800

    terminal mode exit keybind

--- lua/marcus/terminal.lua
+++ lua/marcus/terminal.lua
@@ -1,3 +1,3 @@
 vim.o.shell = 'pwsh'
-vim.keymap.set('n', '<leader>t', ':split | terminal<CR>')
-print('terminal loaded')
+vim.keymap.set('n', '<leader>t', ':split | terminal<CR>', { desc = 'Open terminal window'})
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
